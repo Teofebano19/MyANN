1c79fecb51946f32ef248d428e3f1f24249397fc
andreysima <andrey.simaputera@yahoo.com>
2015-11-21 22:16:34 +0700

    perceptron training rule done, not yet tested

--- src/SinglePerceptron/PerceptronTrainingRule.java
+++ src/SinglePerceptron/PerceptronTrainingRule.java
@@ -6,18 +6,84 @@ import java.util.*;
  *
  * @author Andrey
  */
-public class PerceptronTrainingRule {
+public final class PerceptronTrainingRule {
     public static Double learningrate = 0.1;
     public static int maxepoch = 10;
-    public static Double errortreshold = 0.1;
-    public SingleNeuronLayer neuronlayer;
+    public static Double errortreshold = 0.01;
     public boolean isConvergent;
-    private int currentepoch;
-    
+    private int currentepoch;   
+    public Double realoutput;
+    public Double output;
+    public Double error;
+    public Double targetminoutput;
+    public Double deltaweight;
+    public Double updatedweight;
+    SingleNeuron neuron = new SingleNeuron();    
+ 
     PerceptronTrainingRule(){
+        currentepoch = 1;
+        realoutput = 0.0;
+        
         while(currentepoch <= maxepoch || isConvergent){
-            
+            for(int i = 0; i < neuron.numinstances; i++){
+                CountRealOutput(i);
+                output = SignActivationFunction(realoutput);
+                realoutput = 0.0;
+                targetminoutput = neuron.targets.elementAt(i) - output;
+                for(int j = 0; j < neuron.numinputs; j++){
+                    deltaweight = learningrate * targetminoutput * neuron.data.elementAt(i).elementAt(j); 
+                    updatedweight = neuron.weights.elementAt(j) + deltaweight;
+                    neuron.weights.setElementAt(updatedweight,j);
+                }
+            }
+            CountError();
+            if(error < errortreshold){
+                isConvergent = true;
+            }
+            currentepoch++;
         }
+    }
+    
+    public void CountRealOutput(int d){
+        for(int i = 0; i < neuron.numinputs; i++){    
+            realoutput += neuron.data.elementAt(d).elementAt(i) * neuron.weights.elementAt(i);
+        }
+    }
+    
+    public void CountError(){
+        for(int i = 0; i < neuron.numinstances; i++){
+            for(int j = 0; j < neuron.numinputs; j++){
+                realoutput += neuron.data.elementAt(i).elementAt(j) * neuron.weights.elementAt(j);
+            }
+            output = SignActivationFunction(realoutput);
+            realoutput = 0.0;
+            targetminoutput = neuron.targets.elementAt(i) - output;
+            //count error
+            error += targetminoutput*targetminoutput;
+        }
+        error = error/2;
+    }
+    
+    public Double SigmoidActivationFunction(Double d){
+        return 0.0;
+    }
+
+    public Double SignActivationFunction(Double d){
+        if(d >= 0){
+            return 1.0;
+        }
+        else{
+            return -1.0;
+        }       
+    }
+    
+    public Double StepActivationFunction(Double d){
+        if(d >= 0){
+            return 1.0;
+        }
+        else{
+            return 0.0;
+        }    
     }
 }
 
--- src/SinglePerceptron/SingleNeuron.java
+++ src/SinglePerceptron/SingleNeuron.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package SinglePerceptron;
 
-import java.util.Random;
 import java.util.Vector;
 
 /**
@@ -13,11 +7,43 @@ import java.util.Vector;
  * @author Andrey
  */
 public class SingleNeuron {
-    // Attributes
+    public int numinstances;
+    public int numinputs;
     public Vector<Double> weights;
-    public Double target;
+    public Vector<Double> inputs;
+    public Vector<Double> targets;
+    public Vector<Vector<Double>> data;
     
     // CTOR
     SingleNeuron(){
+        this.numinstances = 3;
+        this.numinputs = 4;//termasuk bias
+        
+        inputs.add(1.0);//bias
+        inputs.add(1.0);
+        inputs.add(0.0);
+        inputs.add(1.0);
+        data.add(inputs);
+        
+        inputs.add(1.0);//bias
+        inputs.add(0.0);
+        inputs.add(-1.0);
+        inputs.add(-1.0);
+        data.add(inputs);
+        
+        inputs.add(1.0);//bias
+        inputs.add(-1.0);
+        inputs.add(-0.5);
+        inputs.add(-1.0);
+        data.add(inputs);
+        
+        weights.add(0.0);
+        weights.add(0.0);
+        weights.add(0.0);
+        weights.add(0.0);   
+        
+        targets.add(-1.0);
+        targets.add(1.0);
+        targets.add(1.0);
     }
 }
